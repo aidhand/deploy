332ab7ee84688efbe99105d8de7b0d9f2c44d66a
Aidhan Dossel <me@aidhan.net>
2024-10-20 18:01:17 +0800

    Add a template

---
+++ apps/template/app.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import "@unocss/reset/tailwind.css";
+
+  import "~/assets/css/globals.css";
+</script>
+
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
