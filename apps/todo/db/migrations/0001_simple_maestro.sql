d9a5c9b5a0ddb2c2ea055b539888866f99240f9b
Aidhan Dossel <me@aidhan.net>
2024-12-07 16:35:25 +0800

    Remove todo app

--- apps/todo/db/migrations/0001_simple_maestro.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS "todos" (
-	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "todos_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
-	"name" varchar(255) NOT NULL,
-	"completed" boolean DEFAULT false NOT NULL
-);
